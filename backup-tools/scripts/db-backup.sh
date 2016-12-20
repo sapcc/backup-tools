9fb2ad690ce90a4ea364568c3ed4317f907fc825
Norbert Tretkowski <norbert.tretkowski@sap.com>
2016-12-20 11:09:21 +0100

    Add Dockerfile, crontab and backup-script

---
+++ backup-tools/scripts/db-backup.sh
@@ -0,0 +1,120 @@
+#!/bin/sh
+
+TESTING=0
+PG_DUMP=1
+
+SWIFT_CONTAINER="BACKUP-${MY_POD_NAME}-${MY_POD_NAMESPACE}"
+
+# We assume that the databases are using their default ports
+MYSQL_PORT=3306
+MYSQL_SOCKET=/db/socket/mysqld.sock
+PGSQL_PORT=5432
+PGSQL_SOCKET=/db/socket/.s.PGSQL.$PGSQL_PORT
+PGSQL_BARMAN_DIR=/var/lib/barman/
+
+CUR_TS="$(date +%Y%m%d%H%M)"
+LAST_BACKUP_FILE="/tmp/last_backup_timestap"
+LOCKFILE="/tmp/backup.lock"
+
+if [ -f "$LAST_BACKUP_FILE" ] ; then
+  LAST_BACKUP_TS="$(cat $LAST_BACKUP_FILE)"
+else
+  LAST_BACKUP_TS=0
+fi
+
+if [ -f "$LOCKFILE" ] ; then
+  echo "Backup in progress..."
+  exit 0
+fi
+
+if [ -f "/etc/db-backup/mysql.backup.full" ] && [ -S $MYSQL_SOCKET ] ; then
+
+  DATADIR=/db/data/
+  SOCKET=/db/socket/mysqld.sock
+  BACKUP_BASE=/backup/MySQL/base/
+  USERNAME=root
+  PASSWORD=foobar
+
+  # Create backup interval
+  INTERVAL_FULL="$(cat /etc/db-backup/mysql.backup.full)"
+  INTERVAL_INCR="$(cat /etc/db-backup/mysql.backup.incr)"
+  IS_NEXT_TS_FULL="$(date --date="now - $INTERVAL_FULL" +%Y%m%d%H%M)"
+  IS_NEXT_TS_INCR="$(date --date="now - $INTERVAL_INCR" +%Y%m%d%H%M)"
+
+  if [ "$TESTING" -gt "0" ] ; then
+    # Cleanup for testing
+    rm -rf /backup/*
+  fi
+
+  echo "$IS_NEXT_TS_FULL -ge $LAST_BACKUP_TS"
+  if [ "$IS_NEXT_TS_FULL" -ge "$LAST_BACKUP_TS" ] ; then
+    touch $LOCKFILE
+    echo "$CUR_TS" > $LAST_BACKUP_FILE
+
+    # MySQL Backup (full)
+    xtrabackup --backup --user=$USERNAME --password=$PASSWORD --target-dir=$BACKUP_BASE --datadir=$DATADIR --socket=$MYSQL_SOCKET
+    swift upload "mariadb-$SWIFT_CONTAINER/base" $BACKUP_BASE
+
+    rm $LOCKFILE
+    exit 0
+  fi
+
+  if [ "$IS_NEXT_TS_INCR" -ge "$LAST_BACKUP_TS" ] ; then
+    touch $LOCKFILE
+    echo "$CUR_TS" > $LAST_BACKUP_FILE
+
+    if [ -d "/backup/inc$LAST_BACKUP_TS" ] ; then
+      BACKUP_BASE=/backup/inc$LAST_BACKUP_TS
+    fi
+
+    # MySQL Backup (incremental)
+
+    xtrabackup --backup --user=$USERNAME --password=$PASSWORD --target-dir=/backup/inc$CUR_TS --incremental-basedir=$BACKUP_BASE --datadir=$DATADIR --socket=$MYSQL_SOCKET
+    swift upload "mariadb-$SWIFT_CONTAINER/inc$CUR_TS" /backup/inc$CUR_TS
+    rm $LOCKFILE
+    exit 0
+  fi
+
+fi
+
+if [ -f "/etc/db-backup/postgres.backup.full" ] ; then
+  # PostgreSQL Backup
+  DATADIR=/db/data
+  BACKUP_BASE=/backup/pgsql/base
+
+  if [ ! -d "$BACKUP_BASE" ] ; then
+    mkdir -p "$BACKUP_BASE"
+  fi
+
+  # Create backup interval
+  INTERVAL_FULL="$(cat /etc/db-backup/postgres.backup.full)"
+  IS_NEXT_TS_FULL="$(date --date="now - $INTERVAL_FULL" +%Y%m%d%H%M)"
+
+  if [ "$TESTING" -gt "0" ] ; then
+    # Cleanup for testing
+    rm -rf /backup/*
+  fi
+
+  echo "$IS_NEXT_TS_FULL -ge $LAST_BACKUP_TS"
+
+  if [ "$IS_NEXT_TS_FULL" -ge "$LAST_BACKUP_TS" ] ; then
+
+
+    touch $LOCKFILE
+    echo "$CUR_TS" > $LAST_BACKUP_FILE
+
+    if [ "$PG_DUMP" = 1 ] ; then
+      for i in `psql -q -A -t -c "SELECT datname FROM pg_database" -h localhost -U postgres | grep -E -v "(^template|^postgres$)"` ; do pg_dump -U postgres -h localhost $i --file=$BACKUP_BASE/$i.sql ; gzip $BACKUP_BASE/$i.sql ; done
+      swift upload --changed "postgres-$SWIFT_CONTAINER/$CUR_TS" $BACKUP_BASE
+    else
+      # Postgres Backup (full)
+      /usr/bin/barman  cron
+      /usr/bin/barman backup all
+      swift upload --changed "postgres-$SWIFT_CONTAINER/$CUR_TS" $BACKUP_BASE
+      swift upload --changed "postgres-$SWIFT_CONTAINER/WAL" $PGSQL_BARMAN_DIR
+    fi
+
+    rm $LOCKFILE
+    exit 0
+  fi
+fi
