0e326734b05bb9d559ac57f7472c789f75e1c28b
Josef Fröhle <github@josef-froehle.de>
2017-02-24 09:30:26 +0100

    EXPIRATION_INTERVAL

--- backup-tools/backup-image/files/backup-run.sh
+++ backup-tools/backup-image/files/backup-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "declare -x MY_POD_NAME=\"${MY_POD_NAME}\"" > /env.cron
 echo "declare -x MY_POD_NAMESPACE=\"${MY_POD_NAMESPACE}\"" >> /env.cron
+echo "declare -x EXPIRATION_INTERVAL=\"${EXPIRATION_INTERVAL}\"" >> /env.cron
 echo "declare -x OS_AUTH_URL=\"${OS_AUTH_URL}\"" >> /env.cron
 echo "declare -x OS_AUTH_VERSION=\"${OS_AUTH_VERSION}\"" >> /env.cron
 echo "declare -x OS_IDENTITY_API_VERSION=\"${OS_IDENTITY_API_VERSION}\"" >> /env.cron
