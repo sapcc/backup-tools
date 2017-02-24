4254a2e214c199e0bf1ab05fec5b7fd0a201b687
Norbert Tretkowski <norbert.tretkowski@sap.com>
2017-02-24 12:01:47 +0100

    Do not use a separate container for replicated backups

--- backup-tools/repl-image/files/backup-replication.sh
+++ backup-tools/repl-image/files/backup-replication.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-source /env.cron
+source /env_staging.cron
 
 LOGFILE="/proc/1/fd/1"
+
+REPLICATE_FROM="$OS_REGION_NAME"
 REPLICATE_TO="eu-de-1"
 
 cd /backup
 
-source /env.cron
-swift list db_backup | grep "^$OS_REGION_NAME/" > /backup/from.log
+source /env_staging.cron
+swift list db_backup | grep "^$REPLICATE_FROM/" > /backup/from.log
 
 source /env_$REPLICATE_TO.cron
-swift list db_backup_replication | grep "^$OS_REGION_NAME/" > /backup/to.log
+swift list db_backup | grep "^$REPLICATE_FROM/" > /backup/to.log
 
 REPL_OBJECTS="`cat /backup/from.log /backup/to.log | sort | uniq -u`"
 
 if [ "$REPL_OBJECTS" != "" ] ; then
-  source /env_$FROM.cron
+  source /env_$REPLICATE_FROM.cron
 
-  echo "[$(date +%Y%m%d%H%M%S)] Downloading backups from $FROM..." > $LOGFILE
+  echo "[$(date +%Y%m%d%H%M%S)] Downloading backups from $REPLICATE_FROM..." > $LOGFILE
   for i in $REPL_OBJECTS ; do
     echo -n "[$(date +%Y%m%d%H%M%S)] " > $LOGFILE
     swift download db_backup $i > $LOGFILE
   done
 
-  source /env_$TO.cron
+  source /env_$REPLICATE_TO.cron
 
-  echo "[$(date +%Y%m%d%H%M%S)] Uploading backups to $TO..." > $LOGFILE
+  echo "[$(date +%Y%m%d%H%M%S)] Uploading backups to $REPLICATE_TO..." > $LOGFILE
   for i in $REPL_OBJECTS ; do
     echo -n "[$(date +%Y%m%d%H%M%S)] " > $LOGFILE
-    swift upload db_backup_replication $i > $LOGFILE
+    swift upload db_backup $i > $LOGFILE
   done
 
   rm -rf /backup/*
