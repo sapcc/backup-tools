54cdee5853a50fd3d50bb31428378f6bc6f27ba4
Norbert Tretkowski <tretkowski@b1-systems.de>
2017-02-28 11:52:02 +0100

    Remove /env.cron

--- backup-tools/repl-image/files/replication-run.sh
+++ backup-tools/repl-image/files/replication-run.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-echo "declare -x MY_POD_NAME=\"${MY_POD_NAME}\"" > /env.cron
-echo "declare -x MY_POD_NAMESPACE=\"${MY_POD_NAMESPACE}\"" >> /env.cron
-echo "declare -x OS_AUTH_URL=\"${OS_AUTH_URL}\"" >> /env.cron
-echo "declare -x OS_AUTH_VERSION=\"${OS_AUTH_VERSION}\"" >> /env.cron
-echo "declare -x OS_IDENTITY_API_VERSION=\"${OS_IDENTITY_API_VERSION}\"" >> /env.cron
-echo "declare -x OS_USERNAME=\"${OS_USERNAME}\"" >> /env.cron
-echo "declare -x OS_USER_DOMAIN_NAME=\"${OS_USER_DOMAIN_NAME}\"" >> /env.cron
-echo "declare -x OS_PROJECT_NAME=\"${OS_PROJECT_NAME}\"" >> /env.cron
-echo "declare -x OS_PROJECT_DOMAIN_NAME=\"${OS_PROJECT_DOMAIN_NAME}\"" >> /env.cron
-echo "declare -x OS_REGION_NAME=\"${OS_REGION_NAME}\"" >> /env.cron
-echo "declare -x OS_PASSWORD=\"${OS_PASSWORD}\"" >> /env.cron
-chmod 600 /env.cron
+chmod 600 /backup/env/*
 cron -f
