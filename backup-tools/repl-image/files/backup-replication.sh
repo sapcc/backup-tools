faba51949866ec58694888beaaab76b0c92c33c3
Norbert Tretkowski <norbert.tretkowski@sap.com>
2017-03-01 14:19:54 +0100

    Fix logging for replication image as well

--- backup-tools/repl-image/files/backup-replication.sh
+++ backup-tools/repl-image/files/backup-replication.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-LOGFILE="/proc/1/fd/1"
-
 if [ ! -f /backup/env/from.env ] || [ ! -f /backup/env/to1.env ] ; then
-  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Configuration files missing, check helm deployment." > $LOGFILE
+  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Configuration files missing, check helm deployment."
   exit 1
 fi
 
@@ -29,21 +27,21 @@ REPL_OBJECTS="`cat /backup/tmp/from.log /backup/tmp/to.log | sort | uniq -u`"
 if [ "$REPL_OBJECTS" != "" ] ; then
   source /backup/env/from.env
 
-  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Downloading backups from $REPLICATE_FROM..." > $LOGFILE
+  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Downloading backups from $REPLICATE_FROM..."
   for i in $REPL_OBJECTS ; do
-    echo -n "$(date +'%Y/%m/%d %H:%M:%S %Z') " > $LOGFILE
-    swift download db_backup $i > $LOGFILE
+    echo -n "$(date +'%Y/%m/%d %H:%M:%S %Z') "
+    swift download db_backup $i
   done
 
   source /backup/env/to1.env
 
-  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Uploading backups to $REPLICATE_TO..." > $LOGFILE
+  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Uploading backups to $REPLICATE_TO..."
   for i in $REPL_OBJECTS ; do
-    echo -n "$(date +'%Y/%m/%d %H:%M:%S %Z') " > $LOGFILE
-    swift upload db_backup $i > $LOGFILE
+    echo -n "$(date +'%Y/%m/%d %H:%M:%S %Z') "
+    swift upload db_backup $i
   done
 
   rm -rf /backup/tmp/*
 else
-  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') No new backups to transfer." > $LOGFILE
+  echo "$(date +'%Y/%m/%d %H:%M:%S %Z') No new backups to transfer."
 fi
