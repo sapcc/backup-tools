90da463d9d98ec7169bd8e232e445bc84238e3ed
Norbert Tretkowski <tretkowski@b1-systems.de>
2017-08-01 11:28:29 +0200

    Fix formatting

--- backup-tools/backup-image/go-src/backup-run.go
+++ backup-tools/backup-image/go-src/backup-run.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
 	"time"
-	"net/http"
-	"log"
 
-	"github.com/urfave/cli"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/urfave/cli"
 )
 
 const (
@@ -21,32 +21,32 @@ const (
 )
 
 var (
-	lastSuccess = prometheus.NewGauge(prometheus.GaugeOpts {
+	lastSuccess = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "backup_last_success",
 		Help: "Unix Timestamp of last successful backup run",
 	})
 
-	lastError = prometheus.NewGauge(prometheus.GaugeOpts {
+	lastError = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "backup_last_error",
 		Help: "Unix Timestamp of last failed backup run",
 	})
 
-	countSuccess = prometheus.NewCounter(prometheus.CounterOpts {
+	countSuccess = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "backup_count_success",
 		Help: "Counter for successful backup runs",
 	})
 
-	countError = prometheus.NewCounter(prometheus.CounterOpts {
+	countError = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "backup_count_error",
 		Help: "Counter for failed backup runs",
 	})
 
-	backupBegin = prometheus.NewGauge(prometheus.GaugeOpts {
+	backupBegin = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "backup_last_start",
 		Help: "Unix Timestamp of last backup start",
 	})
 
-	backupFinish = prometheus.NewGauge(prometheus.GaugeOpts {
+	backupFinish = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "backup_last_finish",
 		Help: "Unix Timestampf of last backup finish",
 	})
@@ -91,7 +91,7 @@ func runServer(c *cli.Context) {
 
 			t, err := ioutil.ReadFile("/tmp/last_backup_timestamp")
 			if err != nil {
-			  fmt.Print(err)
+				fmt.Print(err)
 			}
 			rx := regexp.MustCompile(`^([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})$`)
 			ts := rx.ReplaceAllString(strings.Trim(string(t), "\n"), "$1-$2-$3 $4:$5:00 PM")
@@ -115,9 +115,9 @@ func runServer(c *cli.Context) {
 	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
 	port := os.Getenv("BACKUP_METRICS_PORT")
 	if port == "" {
-	  log.Fatal(http.ListenAndServe(":9188", nil))
+		log.Fatal(http.ListenAndServe(":9188", nil))
 	} else {
-	  log.Fatal(http.ListenAndServe(":" + port, nil))
+		log.Fatal(http.ListenAndServe(":"+port, nil))
 	}
 }
 
