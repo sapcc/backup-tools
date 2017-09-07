2735cb268c8e135f28025307c450528ef875bfec
Josef Fröhle <github@josef-froehle.de>
2017-09-07 16:17:30 +0200

    squash and move Dockerfiles for backup-image (#17)
    Vendoring the source for backup-image and change on makefile

---
+++ backup-tools/backup-image/go-src/vendor/github.com/sapcc/containers/backup-tools/go-src/prometheus/prometheus.go
@@ -0,0 +1,194 @@
+package prometheus
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// Gauge struct for the prometheus
+type Gauge struct {
+	processBegin      prometheus.Gauge
+	processFinish     prometheus.Gauge
+	lastSuccess       prometheus.Gauge
+	lastError         prometheus.Gauge
+	countFilesAll     prometheus.Gauge
+	countFilesCurrent prometheus.Gauge
+	countSuccess      prometheus.Counter
+	countError        prometheus.Counter
+}
+
+var (
+	registry *prometheus.Registry
+)
+
+func init() {
+	registry = prometheus.NewRegistry()
+}
+
+func (g *Gauge) initBackup() {
+	g.lastSuccess = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_last_success",
+		Help: "Unix Timestamp of last successful backup run",
+	})
+
+	g.lastError = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_last_error",
+		Help: "Unix Timestamp of last failed backup run",
+	})
+
+	g.countSuccess = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "backup_count_success",
+		Help: "Counter for successful backup runs",
+	})
+
+	g.countError = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "backup_count_error",
+		Help: "Counter for failed backup runs",
+	})
+
+	g.processBegin = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_last_start",
+		Help: "Unix Timestamp of last backup start",
+	})
+
+	g.processFinish = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_last_finish",
+		Help: "Unix Timestampf of last backup finish",
+	})
+
+	g.registry(false)
+}
+
+func (g *Gauge) initReplication() {
+	g.lastSuccess = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_replication_last_success",
+		Help: "Unix Timestamp of last successful replication run",
+	})
+
+	g.lastError = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_replication_last_error",
+		Help: "Unix Timestamp of last failed replication run",
+	})
+
+	g.countSuccess = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "backup_replication_count_success",
+		Help: "Counter for successful replication runs",
+	})
+
+	g.countError = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "backup_replication_count_error",
+		Help: "Counter for failed replication runs",
+	})
+
+	g.countFilesAll = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_replication_count_files_all",
+		Help: "Counter of all current files to replicate",
+	})
+
+	g.countFilesCurrent = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_replication_count_files_current",
+		Help: "Counter of current files to replicate are done",
+	})
+
+	g.processBegin = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_replication_last_start",
+		Help: "Unix Timestamp of last replication start",
+	})
+
+	g.processFinish = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "backup_replication_last_finish",
+		Help: "Unix Timestampf of last replication finish",
+	})
+
+	g.registry(true)
+}
+
+func (g *Gauge) registry(replicate bool) {
+	if replicate {
+		registry.MustRegister(g.countFilesAll)
+		registry.MustRegister(g.countFilesCurrent)
+	}
+	registry.MustRegister(g.countError)
+	registry.MustRegister(g.countSuccess)
+	registry.MustRegister(g.lastError)
+	registry.MustRegister(g.lastSuccess)
+	registry.MustRegister(g.processBegin)
+	registry.MustRegister(g.processFinish)
+}
+
+func (g *Gauge) initLast(replicate bool) {
+	g.lastError.Set(0)
+	g.lastSuccess.Set(0)
+	if replicate {
+		g.countFilesAll.Set(0)
+		g.countFilesCurrent.Set(0)
+	}
+}
+
+// Beginn set the start time of the process
+func (g *Gauge) Beginn() {
+	g.processBegin.Set(float64(time.Now().Unix()))
+}
+
+// AllFiles set the start time of the process
+func (g *Gauge) AllFiles(filesCount int) {
+	g.countFilesAll.Set(float64(filesCount))
+}
+
+// CurrentFile set the start time of the process
+func (g *Gauge) CurrentFile(fileId int) {
+	g.countFilesCurrent.Set(float64(fileId))
+}
+
+// Finish set the finish time of the process
+func (g *Gauge) Finish() {
+	g.processFinish.Set(float64(time.Now().Unix()))
+}
+
+// SetError set the current time for lastError and incement the countError
+func (g *Gauge) SetError() {
+	g.lastError.Set(float64(time.Now().Unix()))
+	g.countError.Inc()
+}
+
+// SetSuccess set the current time for lastSuccess and incement the countSuccess
+func (g *Gauge) SetSuccess(ts *time.Time) {
+	if ts != nil {
+		g.lastSuccess.Set(float64(ts.Unix()))
+	} else {
+		g.lastSuccess.Set(float64(time.Now().Unix()))
+	}
+	g.countSuccess.Inc()
+}
+
+// ServerStart starts the prometheus metrics server
+func (g *Gauge) ServerStart() {
+	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+	port := os.Getenv("BACKUP_METRICS_PORT")
+	if port == "" {
+		log.Fatal(http.ListenAndServe(":9188", nil))
+	} else {
+		log.Fatal(http.ListenAndServe(":"+port, nil))
+	}
+}
+
+// NewBackup create a Gauge object for Backup
+func NewBackup() *Gauge {
+	gauge := new(Gauge)
+	gauge.initBackup()
+	gauge.initLast(false)
+	return gauge
+}
+
+// NewReplication create a Gauge object for Replication
+func NewReplication() *Gauge {
+	gauge := new(Gauge)
+	gauge.initReplication()
+	gauge.initLast(true)
+	return gauge
+}
