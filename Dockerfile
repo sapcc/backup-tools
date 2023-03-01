547b1c5a3c991f928a94df7d0169b4e4d722ba82
Stefan Majewsky <stefan.majewsky@sap.com>
2023-03-01 15:20:27 +0100

    rename backup-tools binary to backup-server
    I will add a simple HTTP API to this process to enable the automated
    test tooling to interrogate it in a reliable way.

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,14 @@ FROM golang:1.20-alpine3.17 as gobuilder
 
 COPY . /src
 RUN cd /src && go build -ldflags "-s -w" -o /pkg/bin/backup-restore ./cmd/backup-restore
-RUN cd /src && go build -ldflags "-s -w" -o /pkg/bin/backup-tools   .
+RUN cd /src && go build -ldflags "-s -w" -o /pkg/bin/backup-server  .
 
 FROM alpine:3.17
 LABEL source_repository="https://github.com/sapcc/containers"
 ARG POSTGRES_VERSION=12
 
-RUN mkdir /backup \
-	&& apk add --no-cache postgresql${POSTGRES_VERSION}-client ca-certificates less vim iproute2 \
-	&& test -x /usr/bin/psql \
-	&& test -x /usr/bin/pg_dump
+RUN apk add --no-cache --no-progress postgresql${POSTGRES_VERSION}-client ca-certificates curl less vim iproute2
 
 COPY --from=gobuilder /pkg/bin/ /usr/local/sbin/
 
-VOLUME ["/backup"]
-CMD ["/usr/local/sbin/backup-tools", "create"]
+CMD ["/usr/local/sbin/backup-server"]
--- main.go
+++ main.go
@@ -20,48 +20,197 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
 
+	"github.com/kballard/go-shellquote"
+	"github.com/majewsky/schwift"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sapcc/containers/internal/core"
+	"github.com/sapcc/containers/internal/prometheus"
 	"github.com/sapcc/go-api-declarations/bininfo"
 	"github.com/sapcc/go-bits/httpext"
 	"github.com/sapcc/go-bits/logg"
+	"github.com/sapcc/go-bits/must"
 )
-
-func usage() {
-	fmt.Fprintf(os.Stderr, "USAGE: %s restore              - Restore from a backup stored in the regional Swift.\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "       %s restore-crossregion  - Restore from a backup stored in a different Swift.\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "       %s credentials          - Show credentials for a cross-regional restore.\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "       %s manual               - Restore from a backup file on the local disk.\n", os.Args[0])
-}
 
 func main() {
 	wrap := httpext.WrapTransport(&http.DefaultTransport)
 	wrap.SetOverrideUserAgent(bininfo.Component(), bininfo.VersionOr("unknown"))
 
-	switch len(os.Args) {
-	case 1:
-		usage()
-		return
-	case 2:
-		switch os.Args[1] {
-		case "create": //NOTE: This subcommand is not shown in usage() because it's not meant for interactive use.
-			commandCreateBackup()
-		case "restore":
-			logg.Fatal("not implemented yet, please use `backup-restore` for now") //TODO
-		case "restore-crossregion":
-			logg.Fatal("not implemented yet, please use `backup-restore` for now") //TODO
-		case "credentials":
-			logg.Fatal("not implemented yet, please use `backup-restore` for now") //TODO
-		case "manual":
-			logg.Fatal("not implemented yet, please use `backup-restore` for now") //TODO
-		default: //invalid subcommand
-			usage()
-			os.Exit(1)
+	ctx := httpext.ContextWithSIGINT(context.Background(), 1*time.Second)
+	cfg := must.Return(core.NewConfiguration())
+
+	//listen to SIGUSR1 (this signal causes a backup to be created immediately, regardless of schedule)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGUSR1)
+
+	//fork off the main loop
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ticker := time.NewTicker(1 * time.Minute)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				err := createBackupIfNecessary(cfg)
+				if err != nil {
+					logg.Error(err.Error())
+				}
+			case <-signalChan:
+				err := createBackup(cfg, "because of user request (SIGUSR1)")
+				if err != nil {
+					logg.Error(err.Error())
+				}
+			}
 		}
-	default: //invalid number of args
-		usage()
-		os.Exit(1)
+	}()
+
+	//serve Prometheus metrics on the main thread
+	prometheus.InitMetrics()
+	http.Handle("/metrics", promhttp.Handler())
+	must.Succeed(httpext.ListenAndServeContext(ctx, cfg.ListenAddress, nil))
+
+	//on SIGINT/SIGTERM, give the backup main loop a chance to complete a backup that's currently in flight
+	wg.Wait()
+}
+
+const (
+	// A time format corresponding to YYYYMMDDHHMM without any separator chars.
+	backupTimeFormat = "200601021504"
+	// Segment size for Swift uploads
+	uploadSegmentSize = 2 << 30 // 2 GiB
+	// Retention time for backups.
+	retentionTime time.Duration = 10 * 24 * time.Hour // 10 days
+)
+
+func createBackupIfNecessary(cfg *core.Configuration) error {
+	//check last_backup_timestamp to see if a backup is needed
+	lastTimeObj := cfg.Container.Object(cfg.ObjectNamePrefix + "last_backup_timestamp")
+	lastTime, err := readLastBackupTimestamp(lastTimeObj)
+	if err != nil {
+		return err
 	}
+	if lastTime.Add(cfg.Interval).After(time.Now()) {
+		prometheus.SetSuccess(lastTime)
+		return nil
+	}
+
+	return createBackup(cfg, "because of schedule")
+}
+
+func readLastBackupTimestamp(obj *schwift.Object) (time.Time, error) {
+	str, err := obj.Download(nil).AsString()
+	if err != nil {
+		if schwift.Is(err, http.StatusNotFound) {
+			//this branch is esp. relevant for the first ever backup -> we just report a very old last backup to force a backup immediately
+			return time.Unix(0, 0).UTC(), nil
+		}
+		return time.Time{}, err
+	}
+	t, err := time.Parse(backupTimeFormat, str)
+	if err != nil {
+		//recover from malformed timestamp files by forcing a new backup immediately, same as above
+		return time.Unix(0, 0).UTC(), nil
+	}
+	return t, nil
+}
+
+func createBackup(cfg *core.Configuration, reason string) (returnedError error) {
+	//track metrics for this backup
+	nowTime := time.Now()
+	nowTimeStr := nowTime.Format(backupTimeFormat)
+	prometheus.Begin()
+	defer func() {
+		if returnedError == nil {
+			prometheus.SetSuccess(nowTime)
+		} else {
+			prometheus.SetError()
+		}
+		prometheus.Finish()
+	}()
+	logg.Info("creating backup %s%s %s...", cfg.ObjectNamePrefix, nowTimeStr, reason)
+
+	//enumerate databases that need to be backed up
+	cmd := exec.Command("psql",
+		"-qAt", "-h", cfg.PgHostname, "-U", cfg.PgUsername, "-c", //NOTE: PGPASSWORD comes via inherited env variable
+		`SELECT datname FROM pg_database WHERE datname !~ '^template|^postgres$'`)
+	logg.Info(">> " + shellquote.Join(cmd.Args...))
+	cmd.Stderr = os.Stderr
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("could not enumerate databases with psql: %w", err)
+	}
+	databaseNames := strings.Fields(string(output))
+
+	//stream backups from pg_dump into Swift
+	for _, databaseName := range databaseNames {
+		//NOTE: We need to do this in two separate goroutines because we need to
+		//Close() the writer side in order for LargeObject.Append() to return on
+		//the reader side.
+
+		//run pg_dump
+		pipeReader, pipeWriter := io.Pipe()
+		errChan := make(chan error, 1) //must be buffered to ensure that `pipewriter.Close()` runs immediately
+		go func() {
+			defer pipeWriter.Close()
+			cmd := exec.Command("pg_dump",
+				"-h", cfg.PgHostname, "-U", cfg.PgUsername, //NOTE: PGPASSWORD comes via inherited env variable
+				"-c", "--if-exist", "-C", "-Z", "5", databaseName)
+			logg.Info(">> " + shellquote.Join(cmd.Args...))
+			cmd.Stdout = pipeWriter
+			cmd.Stderr = os.Stderr
+			errChan <- cmd.Run()
+		}()
+
+		//upload the outputs of pg_dump into Swift
+		obj := cfg.Container.Object(cfg.ObjectNamePrefix + fmt.Sprintf("%s/backup/pgsql/base/%s.sql.gz", nowTimeStr, databaseName))
+		lo, err := obj.AsNewLargeObject(schwift.SegmentingOptions{
+			Strategy:         schwift.StaticLargeObject,
+			SegmentContainer: cfg.SegmentContainer,
+		}, nil)
+		if err != nil {
+			return fmt.Errorf("could not start upload into Swift: %w", err)
+		}
+
+		hdr := schwift.NewObjectHeaders()
+		hdr.ExpiresAt().Set(nowTime.Add(retentionTime))
+		err = lo.Append(pipeReader, uploadSegmentSize, hdr.ToOpts())
+		if err != nil {
+			return fmt.Errorf("could not write into Swift: %w", err)
+		}
+
+		err = lo.WriteManifest(hdr.ToOpts())
+		if err != nil {
+			return fmt.Errorf("could not finalize upload into Swift: %w", err)
+		}
+
+		//wait for pg_dump to finish
+		err = <-errChan
+		if err != nil {
+			return fmt.Errorf("could not run pg_dump: %w", err)
+		}
+	}
+
+	//write last_backup_timestamp to indicate that this backup is completed successfully
+	lastTimeObj := cfg.Container.Object(cfg.ObjectNamePrefix + "last_backup_timestamp")
+	err = lastTimeObj.Upload(strings.NewReader(nowTimeStr), nil, nil)
+	if err != nil {
+		return fmt.Errorf("could not write last_backup_timestamp into Swift: %w", err)
+	}
+	logg.Info(">> done")
+	return nil
 }
